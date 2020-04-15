333d2fd1a1e41ceb80eec40d51ba9b116243aa12
tennga <tenngawang6080@gmail.com>
2020-04-15 14:56:22 -0400

    added search bar with instante result

--- src/container/SideBarContainer.js
+++ src/container/SideBarContainer.js
@@ -2,17 +2,42 @@ import React from 'react';
 import AllCountry from '../components/AllCountry';
 import './css/SideBarContainer.css';
 
-const SideBarContainer = (props) => {
-    return(
-        <div className="side-bar-container">
-            <h1>COVID-19 Tracker</h1>
-            <h3>All Countries</h3>
-            <AllCountry 
-                currentCountry = {props.currentCountry} 
-                allCountry={props.allCountry} 
-            />
-        </div>
-    )
+class SideBarContainer extends React.Component {
+    state = {
+        term: ""
+    }
+
+    handleChange = (e) => {
+        this.setState({term: e.target.value})
+    }
+
+    filterCountryByTerm = () => {
+        if(this.state.term){
+           return this.props.allCountry.filter((country)=>{
+                return country.country.toUpperCase().includes(this.state.term.toUpperCase())
+            })
+        } else {
+            return this.props.allCountry
+        }
+    }
+    render(){
+        return(
+            <div className="side-bar-container">
+                <h1>COVID-19 Tracker</h1>
+                <h3>All Countries</h3>
+                <input 
+                    id="search-country" 
+                    onChange={this.handleChange}
+                    value={this.state.term}
+                    placeholder="Search by country name"
+                ></input>
+                <AllCountry 
+                    currentCountry = {this.props.currentCountry} 
+                    allCountry={this.filterCountryByTerm()} 
+                />
+            </div>
+        ) 
+    }
 }
 
 export default SideBarContainer;
