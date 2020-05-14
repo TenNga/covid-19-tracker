d8c14b73c01b87812a76bcc043d17315ad9a7a78
tennga <tenngawang6080@gmail.com>
2020-05-14 15:57:42 -0400

    implemented styled-components for DisplayContainer

--- src/container/DisplayContainer.js
+++ src/container/DisplayContainer.js
@@ -1,21 +1,53 @@
 import React from 'react';
-import './css/DisplayContainer.css';
+import styled from 'styled-components';
+
+    const DisplayBody = styled.div`
+        margin: 50px 20px;
+        text-align: center;
+        & img {
+            width: 100%;
+            max-height: 200px;
+            object-fit: contain;
+        }
+    `;
+
+    const Confirmed = styled.span `
+        color: rgb(67, 67, 177);
+    `;
+
+    const Recovered = styled.span `
+        color: rgb(95, 125, 95);
+    `;
+
+    const Deaths = styled.span `
+        color: rgb(215, 110, 110);
+    `;
+
+    const HeaderOne = styled.h1`
+        letter-spacing: 1px;
+    `;
+    const HeaderTwo = styled.h2`
+        letter-spacing: 1px;
+    `;
+
 
 const DisplayContainer = ({currentCountry,totalConfirmed,currentCountryMoreInfo,countryInfo}) => {
     console.log("MOre Information: ", countryInfo)
+
+
     return(
-        <div className="display-container">
+        <DisplayBody>
             {currentCountry && countryInfo? <>
-                <h1>Country: {countryInfo.country}</h1>
-                <h2>confirmed: <span id="confirmed">{countryInfo.confirmed}</span></h2>
-                <h2>Recovered: <span id="recovered">{countryInfo.recovered}</span></h2>
-                <h2>Deaths: <span id="deaths">{countryInfo.deaths}</span></h2>
+                <HeaderOne>Country: {countryInfo.country}</HeaderOne>
+                <HeaderTwo>confirmed: <Confirmed>{countryInfo.confirmed}</Confirmed></HeaderTwo>
+                <h2>Recovered: <Recovered>{countryInfo.recovered}</Recovered></h2>
+                <h2>Deaths: <Deaths>{countryInfo.deaths}</Deaths></h2>
                 {currentCountryMoreInfo? <>
                 <h2>Population: <span>{currentCountryMoreInfo.population}</span></h2>
                 <img src={currentCountryMoreInfo.flag} alt="country flag" /></>
                 : null }
                 </> : <h1>Total Confirmed: {totalConfirmed}</h1>}
-        </div>
+        </DisplayBody>
     )
 }
 
